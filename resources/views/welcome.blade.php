267584ee7f7c3a1f7b9981106a3de0b43cee0b2e
raja <johndoe@example.com>
2021-11-21 22:14:41 +0530

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
-
 <h1>this is the main page
-
+    <?php
+    if(DB::connection()->getPdo())
+    {
+        echo "Successfully connected to the database => "
+                     .DB::connection()->getDatabaseName();
+    }
+?>
 </h1>
-{{ DB::table('keypepole')->get() }}
+{{ DB::table('Event')->get() }}
